d59a1b7a5f306da17c505eda2d494ecc4e865a20
Giovanna Moraes <160680690+Gioomoraes@users.noreply.github.com>
2026-01-25 16:53:37 -0300

    add basic variables test

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 
 function App() {
+
+  const name: string = "Giovanna"
+  const age: number = 18
+  const isStudent: boolean = true
   return (
     <div className="App">
      <h1>Projeto com ts</h1>
+     <h2>Nome: {name}</h2>
+     <p>Idade: {age}</p>
+     {isStudent && <p>Sim está estudando</p>}
     </div>
   );
 }
