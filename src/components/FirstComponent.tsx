8b2cb5db477b599094e932b396b3e219e23ee457
Giovanna Moraes <160680690+Gioomoraes@users.noreply.github.com>
2026-01-25 19:03:58 -0300

    create component

---
+++ src/components/FirstComponent.tsx
@@ -0,0 +1,12 @@
+import React, { ReactElement} from 'react';
+
+
+function FirstComponent(): ReactElement {
+    return (
+        <div>
+            <h1>Criando componente</h1>
+        </div>
+    )
+}
+
+export default FirstComponent;
