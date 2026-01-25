ecc9a6f62b64ea359681b49eb134449b0cbc5794
Giovanna Moraes <160680690+Gioomoraes@users.noreply.github.com>
2026-01-25 18:27:22 -0300

    add typed function to React component

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,17 @@ function App() {
   const name: string = "Giovanna"
   const age: number = 18
   const isStudent: boolean = true
+
+  const userGreeting = (name: string): string => {
+    return `Olá, ${name}!`;
+  };
   return (
     <div className="App">
      <h1>Projeto com ts</h1>
      <h2>Nome: {name}</h2>
      <p>Idade: {age}</p>
      {isStudent && <p>Sim está estudando</p>}
+     <h3>{userGreeting(name)}</h3>
     </div>
   );
 }
