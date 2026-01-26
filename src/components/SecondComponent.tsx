641eeb0ec5d4c8445b93f5fd2efc964b176c8856
Giovanna Moraes <160680690+Gioomoraes@users.noreply.github.com>
2026-01-26 11:01:46 -0300

    add SecondComponent

---
+++ src/components/SecondComponent.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+interface Props {
+
+}
+
+const SecondComponent = (props: Props) => {
+    return (
+        <div>Segundo Componente</div>
+    )
+}
+
+export default SecondComponent
