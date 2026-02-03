c370a55b6af46533486f006521f5659279f5657f
Giovanna Moraes <160680690+Gioomoraes@users.noreply.github.com>
2026-02-03 10:05:23 -0300

    add State component

---
+++ src/components/State.tsx
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+
+interface Props {}
+
+const State = () => {
+
+const [text, setText] = useState()
+
+  return <div>
+    <p></p>
+  </div>;
+};
+
+export default State;
